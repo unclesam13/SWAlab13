1f5dac425bec57d92db1d223f4170640d3859871
Your Name <you@example.com>
2025-01-26 14:44:21 +0100

    Initial commit

---
+++ src/main/java/com/example/SWAlab13/SwAlab13Application.java
@@ -0,0 +1,13 @@
+package com.example.SWAlab13;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwAlab13Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwAlab13Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/SWAlab13/SwAlab13ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SWAlab13;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwAlab13ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
