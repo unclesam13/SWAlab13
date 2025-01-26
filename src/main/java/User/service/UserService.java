ebea98b774763c839037f1fee33e1ed17ee2e043
Your Name <you@example.com>
2025-01-26 23:22:57 +0100

    Update db and user

---
+++ src/main/java/User/service/UserService.java
@@ -0,0 +1,20 @@
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+    @Autowired
+    private UserRepository userRepository;
+
+    public User createUser(User user) {
+        return userRepository.save(user);
+    }
+
+    public User authenticate(String email, String password) {
+        User user = userRepository.findByEmail(email);
+        if (user != null && user.getPassword().equals(password)) {
+            return user;
+        }
+        throw new RuntimeException("Invalid email or password");
+    }
+}
